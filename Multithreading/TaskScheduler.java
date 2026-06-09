1f47b43e7d7688047d6a99536146df55e1bcff99
Prateek Mohapatra 25BCSH77 <cse.25bcsh77@silicon.ac.in>
2026-06-09 21:07:07 +0530

    Update TaskScheduler.java

--- Multithreading/TaskScheduler.java
+++ Multithreading/TaskScheduler.java
@@ -15,3 +15,46 @@ Each thread = separate class OR Runnable
 Use run() method logic
 Focus on parallel execution behavior
 */
+package day13_progs;
+
+class NumberThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " : " + i);
+        }
+    }
+}
+
+class EvenThread extends Thread {
+    public void run() {
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println(Thread.currentThread().getName() + " : " + i);
+        }
+    }
+}
+
+class OddThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 9; i += 2) {
+            System.out.println(Thread.currentThread().getName() + " : " + i);
+        }
+    }
+}
+
+public class TaskScheduler {
+
+    public static void main(String[] args) {
+
+        NumberThread t1 = new NumberThread();
+        EvenThread t2 = new EvenThread();
+        OddThread t3 = new OddThread();
+
+        t1.setName("Number Thread");
+        t2.setName("Even Thread");
+        t3.setName("Odd Thread");
+
+        t1.start(); // I used start because it is showing error while using run() directly in main() . 
+        t2.start();
+        t3.start();
+    }
+}
